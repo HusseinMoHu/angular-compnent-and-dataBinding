abf19483d8c0236bae94733bc29a0b67e5f1e532
Hussein <husseinm1818@gmail.com>
2021-11-25 21:46:15 +0200

    update

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
 })
 export class ServerElementComponent implements OnInit {
+  // Input decorator make property exposed to the parent component
+  @Input() public element: { name: string; type: string; content: string };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
